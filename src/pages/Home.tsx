e107b395f6c584b496369e271f181aa741bec54d
j-yesung <hisung-ah@kakao.com>
2023-12-28 01:05:39 +0900

    feat: 로그아웃 시, 홈 화면으로 이동

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,29 @@
+import { useEffect } from 'react';
+import { getUserData, getUserSession } from '../API/supabase.api';
+import { useAuth } from '../hooks/userHook/useAuth';
+
 const Home = () => {
-  return <div></div>;
+  // 로그인 세션 테스트 코드
+  const { logout } = useAuth();
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const s = await getUserData();
+      console.log('getUserInfo: ', s);
+    };
+    const getUserToken = async () => {
+      const s = await getUserSession();
+      console.log('getUserToken: ', s);
+    };
+    getUserInfo();
+    getUserToken();
+  }, []);
+
+  return (
+    <>
+      <h1>Home</h1>
+      <button onClick={() => logout()}>로그아웃</button>
+    </>
+  );
 };
 
 export default Home;
