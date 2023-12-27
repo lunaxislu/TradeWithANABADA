46384167c20f2fcb943b17808af2fdf6f3306749
j-yesung <hisung-ah@kakao.com>
2023-12-27 21:30:51 +0900

    feat: Form 컴포넌트 스타일링

--- src/components/user/Form.tsx
+++ src/components/user/Form.tsx
@@ -1,8 +1,7 @@
-import { Link, Navigate, useParams } from 'react-router-dom';
+import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
 import { resetPasswordHandler, signInWithProvider } from '../../API/supabase.api';
 import { useAuth } from '../../hooks/userHook/useAuth';
 import { useInput } from '../../hooks/userHook/useInput';
-import { Button } from '../ui/ButtonCopy';
 import * as St from './Form.styled';
 
 /**
@@ -14,8 +13,9 @@ import * as St from './Form.styled';
  * @returns 회원가입, 로그인, 비밀번호 메일 전송, 비밀번호 변경 Form
  */
 const Form = () => {
-  const { signup, login, updatePassword } = useAuth();
   const params = useParams();
+  const navigate = useNavigate();
+  const { signup, login, updatePassword } = useAuth();
   const { value, onChange, reset, isValid, emailErrorMessage, passwordErrorMessage } = useInput({
     email: '',
     password: '',
@@ -30,7 +30,10 @@ const Form = () => {
           case 'login':
             return (
               <>
-                <h1>ANABADA</h1>
+                <St.Logo>
+                  <Link to="/">ANABADA</Link>
+                </St.Logo>
+                {/* <St.Logo onClick={() => navigate('/')}>ANABADA</St.Logo> */}
                 <form>
                   <input type="email" name="email" placeholder="이메일" value={value.email} onChange={onChange} />
                   {emailErrorMessage && <span>{emailErrorMessage}</span>}
@@ -45,33 +48,58 @@ const Form = () => {
                   />
                 </form>
                 {/* Button 컴포넌트 사용해야 합니다. */}
-                <button
-                  type="submit"
-                  disabled={!isValid}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    reset();
-                    login(value);
-                  }}
-                >
-                  로그인
-                </button>
-                {/* 구글, 카카오 로고 넣어야 합니다. */}
-                <button onClick={() => signInWithProvider('google')}>구글</button>
-                <button onClick={() => signInWithProvider('kakao')}>카카오</button>
-                <Link to="/auth/signup" onClick={reset}>
-                  회원가입
-                </Link>
-                <br />
-                <Link to="/auth/sendMail" onClick={reset}>
-                  비밀번호 변경
-                </Link>
+                <St.ButtonWrapper $active={!isValid}>
+                  <button
+                    type="submit"
+                    disabled={!isValid}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      reset();
+                      login(value);
+                    }}
+                  >
+                    로그인
+                  </button>
+                  <div>
+                    비밀번호를 잊으셨나요?
+                    <Link to="/auth/sendMail" onClick={reset}>
+                      비밀번호 찾기
+                    </Link>
+                  </div>
+                  <St.SocialButtonWrapper>
+                    <img
+                      src="https://developers.google.com/static/identity/images/g-logo.png?hl=ko"
+                      alt=""
+                      onClick={() => signInWithProvider('google')}
+                    />
+                    <img
+                      src={process.env.PUBLIC_URL + '/images/k-logo.png'}
+                      alt=""
+                      onClick={() => signInWithProvider('kakao')}
+                    />
+                  </St.SocialButtonWrapper>
+                  <St.CaptionWrapper>
+                    <St.P1>아직 회원이 아니신가요?</St.P1>
+                    <St.P2>ANABADA와 함께</St.P2>
+                    <St.P2>안 쓰는 물건을 새 물건으로 교환해 보세요</St.P2>
+                    <div
+                      onClick={() => {
+                        navigate('/auth/signup');
+                        reset();
+                      }}
+                    >
+                      회원가입
+                    </div>
+                  </St.CaptionWrapper>
+                </St.ButtonWrapper>
               </>
             );
           case 'signup':
             return (
               <div>
-                <h1>ANABADA</h1>
+                <St.Logo>
+                  <Link to="/">ANABADA</Link>
+                </St.Logo>
                 <form>
                   <input
                     type="email"
@@ -105,14 +133,10 @@ const Form = () => {
                   {passwordErrorMessage && <span>{passwordErrorMessage}</span>}
                   <input type="text" name="name" placeholder="닉네임" value={value.name} onChange={onChange} required />
                 </form>
-                {/* Button 컴포넌트 사용해야 합니다. */}
-                <St.ButtonWrapper>
-                  <Button
+                <St.ButtonWrapper $active={!isValid}>
+                  <button
                     type="submit"
-                    color="black"
                     disabled={!isValid}
-                    active={!isValid}
-                    btnType="auth"
                     onClick={(e) => {
                       e.preventDefault();
                       reset();
@@ -120,49 +144,67 @@ const Form = () => {
                     }}
                   >
                     회원가입
-                  </Button>
-                  <div>
-                    <St.GoogleButton
+                  </button>
+                  <St.SocialButtonWrapper>
+                    <img
                       src="https://developers.google.com/static/identity/images/g-logo.png?hl=ko"
                       alt=""
                       onClick={() => signInWithProvider('google')}
                     />
-                    <St.KakaoButton
+                    <img
                       src={process.env.PUBLIC_URL + '/images/k-logo.png'}
                       alt=""
                       onClick={() => signInWithProvider('kakao')}
                     />
-                  </div>
-                  <Link to="/auth/login" onClick={reset}>
-                    로그인
-                  </Link>
+                  </St.SocialButtonWrapper>
+                  <St.CaptionWrapper>
+                    <St.P1>이미 회원이신가요?</St.P1>
+                    <St.P2>ANABADA와 함께</St.P2>
+                    <St.P2>안 쓰는 물건을 새 물건으로 교환해 보세요</St.P2>
+                    <div
+                      onClick={() => {
+                        navigate('/auth/login');
+                        reset();
+                      }}
+                    >
+                      로그인
+                    </div>
+                  </St.CaptionWrapper>
                 </St.ButtonWrapper>
               </div>
             );
           case 'sendMail':
             return (
               <div>
-                <h1>비밀번호 변경 메일 보내기</h1>
+                <St.Logo>
+                  <Link to="/">ANABADA</Link>
+                </St.Logo>
+                <h2>비밀번호 변경 메일 보내기</h2>
                 <form>
                   <input type="email" name="email" placeholder="이메일" value={value.email} onChange={onChange} />
                   {emailErrorMessage && <span>{emailErrorMessage}</span>}
                 </form>
-                <button
-                  type="submit"
-                  disabled={!isValid}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    reset();
-                    resetPasswordHandler(value);
-                  }}
-                >
-                  변경
-                </button>
+                <St.ButtonWrapper $active={!isValid}>
+                  <button
+                    type="submit"
+                    disabled={!isValid}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      reset();
+                      resetPasswordHandler(value);
+                    }}
+                  >
+                    이메일 전송
+                  </button>
+                </St.ButtonWrapper>
               </div>
             );
           case 'reset':
             return (
               <div>
+                <St.Logo>
+                  <Link to="/">ANABADA</Link>
+                </St.Logo>
                 <h1>비밀번호 변경 페이지</h1>
                 <form>
                   <input
