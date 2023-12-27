167ca23ff35b8f8a4bf4a320e8aeac04d340f26b
Kim MyungSub <audtjqxx@naver.com>
2023-12-27 17:21:34 +0900

    chore: title 클릭시 home으로 navigate 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import HeaderBtnArea from './header/HeaderBtnArea';
 import SearchCategoryArea from './header/SearchCategoryArea';
 import * as St from './header/header.styled';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate('/');
+  };
   return (
     <St.Header>
-      <h1>ANABADA</h1>
+      <h1 onClick={navigateToHome}>ANABADA</h1>
       <SearchCategoryArea />
       <HeaderBtnArea />
     </St.Header>
