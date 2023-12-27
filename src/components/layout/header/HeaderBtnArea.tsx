af5540a29e3a36d675aaeeb09aa19af99d932992
Kim MyungSub <audtjqxx@naver.com>
2023-12-27 17:18:29 +0900

    feat: header 로그인 세션 정보에 따라 버튼 조건부 처리

--- src/components/layout/header/HeaderBtnArea.tsx
+++ src/components/layout/header/HeaderBtnArea.tsx
@@ -1,22 +1,71 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUserSession } from '../../../API/supabase.api';
+import { useAuth } from '../../../hooks/userHook/useAuth';
 import { Button } from '../../ui/Button';
 import * as St from './header.styled';
 
 const HeaderBtnArea = () => {
-  // 로그인 상태 (테스트용)
-  const currentLoginStatus = true;
+  const navigate = useNavigate();
 
-  const dummyEvent = () => {};
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  const { logout } = useAuth();
+
+  const headerNavigateHandler = (path: string) => {
+    navigate(`/${path}`);
+  };
+
+  const checkUserSession = async () => {
+    const userSession = await getUserSession();
+    console.log(userSession);
+    console.log(!!userSession.session);
+    setIsLogin(!!userSession.session);
+  };
+
+  useEffect(() => {
+    checkUserSession();
+  }, []);
+
   const headerButton = [
-    { text: '등록하기', isLogin: true, clickHandler: dummyEvent },
-    { text: '내정보', isLogin: true, clickHandler: dummyEvent },
-    { text: 'Talk', isLogin: true, clickHandler: dummyEvent },
-    { text: '로그인', isLogin: true, clickHandler: dummyEvent },
-    { text: '로그아웃', isLogin: false, clickHandler: dummyEvent },
+    {
+      text: '등록하기',
+      isLogin: true,
+      clickHandler: () => {
+        headerNavigateHandler('write');
+      },
+    },
+    {
+      text: '내정보',
+      isLogin: true,
+      clickHandler: () => {
+        headerNavigateHandler('profile');
+      },
+    },
+    {
+      text: 'Talk',
+      isLogin: true,
+      clickHandler: () => {
+        headerNavigateHandler('');
+      },
+    },
+    {
+      text: '로그아웃',
+      isLogin: true,
+      clickHandler: logout,
+    },
+    {
+      text: '로그인',
+      isLogin: false,
+      clickHandler: () => {
+        headerNavigateHandler('auth/login');
+      },
+    },
   ];
   return (
     <St.HeaderBtnSection>
       {headerButton
-        .filter((btn) => btn.isLogin === currentLoginStatus)
+        .filter((btn) => btn.isLogin === isLogin)
         .map((btn, index) => (
           <Button key={index} color="default" onClick={btn.clickHandler}>
             {btn.text}
