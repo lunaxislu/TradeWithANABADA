c6cc25f6ee23b6075395ac31c020007581e102b7
yeonna18k <yeonna18k@gmail.com>
2023-12-27 20:53:27 +0900

    feat: Profile css 완료

---
+++ src/components/profile/List.styled.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const ListWrapper = styled.div`
+  display: flex;
+`;
+
+export const ListBox = styled.div`
+  width: 25%;
+  border: 0.2rem solid #dcdcdc;
+  ${({ theme }) => theme.mediaQuery.sm`
+  width: 25%;
+  `}
+  ${({ theme }) => theme.mediaQuery.lg`
+  `};
+`;
+
+export const ListImage = styled.div`
+  position: relative;
+`;
+
+export const ListImageProduct = styled.img``;
+
+export const ListImageHeart = styled.img`
+  width: 2rem;
+  position: absolute;
+  bottom: 0.5rem;
+  right: 0.5rem;
+  ${({ theme }) => theme.mediaQuery.lg`
+    width: 2.5rem;
+  `};
+`;
+
+export const ListInfo = styled.div`
+  padding: 0.8rem;
+  line-height: 2.5rem;
+`;
+
+export const ListPriceandDate = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ListTitle = styled.div`
+  font-size: 1.5rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const ListPrice = styled.div`
+  font-size: 1.5rem;
+`;
+
+export const ListDate = styled.div``;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,36 +1,21 @@
 import styled from 'styled-components';
+import ProductList from '../components/profile/ProductList';
+import UserInfo from '../components/profile/UserInfo';
 
 const Profile = () => {
   return (
     <Container>
-      <ProfileReviewWrapper>
-        <ProfileBox>
-          <ProfileImg>
-            <img src={process.env.PUBLIC_URL + '/profile.jpeg'} />
-          </ProfileImg>
-          <ProfileInfo>
-            <Nickname>
-              <p>강나연</p>
-              <button>닉네임 변경하기</button>
-            </Nickname>
-            <ProfileEdit>프로필 변경하기</ProfileEdit>
-          </ProfileInfo>
-        </ProfileBox>
-        <ReviewBox>
-          <p>나의 거래 후기</p>
-        </ReviewBox>
-      </ProfileReviewWrapper>
-
-      <p>찜 목록</p>
-      <LikesList></LikesList>
-      <p>나의 판매 목록</p>
-      <ProductList></ProductList>
+      <UserInfo />
+      <ProductList title={'likes'} />
+      <ProductList title={'products'} />
     </Container>
   );
 };
 
 const Container = styled.div`
+  box-sizing: border-box;
   margin: 3rem auto;
+  padding: 3rem;
   border: 0.2rem solid #dcdcdc;
   height: 100vh;
   width: 50rem;
@@ -41,64 +26,5 @@ const Container = styled.div`
       width : 100rem;
   `}
 `;
-const ProfileReviewWrapper = styled.div`
-  ${({ theme }) => theme.mediaQuery.sm`
-        display: flex;
-  `}
-`;
-const ProfileBox = styled.div`
-  display: flex;
-  margin: 0 auto;
-  border: solid 0.2rem #dcdcdc;
-  width: 48rem;
-  ${({ theme }) => theme.mediaQuery.sm`
-      width : 35rem;
-  `}
-  ${({ theme }) => theme.mediaQuery.lg`
-      width : 45rem;
-  `}
-`;
-const ProfileImg = styled.div`
-  width: 20rem;
-  ${({ theme }) => theme.mediaQuery.sm`
-      width : 17.5rem;
-  `}
-  ${({ theme }) => theme.mediaQuery.lg`
-      width : 22.5rem;
-  `}
-`;
-const ProfileInfo = styled.div`
-  ${({ theme }) => theme.mediaQuery.lg`
-width : 200px;
-margin: 0 auto;
-`}
-`;
-const Nickname = styled.div`
-  & p {
-    font-size: 2rem;
-  }
-  & button {
-    border: none;
-    background-color: transparent;
-  }
-`;
-const ProfileEdit = styled.button`
-  border: none;
-  background-color: transparent;
-`;
-
-const ReviewBox = styled.div`
-  margin: 0 auto;
-  border: 0.2rem solid #dcdcdc;
-  width: 48rem;
-  ${({ theme }) => theme.mediaQuery.sm`
-      width : 35rem;
-  `}
-  ${({ theme }) => theme.mediaQuery.lg`
-      width : 45rem;
-  `}
-`;
-const LikesList = styled.div``;
-const ProductList = styled.div``;
 
 export default Profile;
