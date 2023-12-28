9fc6e5c5cf793fedcccb6f9f5579f5983fb58860
Kim MyungSub <audtjqxx@naver.com>
2023-12-29 03:36:50 +0900

    feat: Card CSS 개선

--- src/components/ui/productCard/ProductCardInfo.tsx
+++ src/components/ui/productCard/ProductCardInfo.tsx
@@ -1,3 +1,4 @@
+import { displayCreateAt } from '../../../utils/date';
 import * as St from './productCard.styled';
 
 type ProductCardInfoProps = {
@@ -7,12 +8,13 @@ type ProductCardInfoProps = {
 };
 
 const ProductCardInfo = ({ createAt, price, title }: ProductCardInfoProps) => {
+  const formatDate = displayCreateAt(createAt);
   return (
     <St.CardInfo>
       <h1>{title}</h1>
       <div>
-        <span>{price}</span>
-        <span>{createAt}</span>
+        <span>{price}원의 가치</span>
+        <span>{formatDate}</span>
       </div>
     </St.CardInfo>
   );
