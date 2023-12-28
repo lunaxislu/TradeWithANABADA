833238db713f92b00e8ccc75f32b9567bb17c9f1
최문길 <cmk0905@naver.com>
2023-12-29 04:02:08 +0900

    feat: 이미지 url받아서 product에 추가 완성함

--- src/components/post/form/PostForm.tsx
+++ src/components/post/form/PostForm.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import { FormEvent, useEffect, useState } from 'react';
 import { getUserSession, supabase } from '../../../API/supabase.api';
 import * as St from './PostForm.styled';
@@ -33,11 +34,10 @@ const PostForm = ({ imgFiles }: PropsType) => {
       imgFiles,
     };
     await insertProduct(product);
-    console.log(product);
-    console.log(imgFiles);
   };
 
   const insertProduct = async (info: ParamForRegist) => {
+    // post Table에 우선 text들을 저장 합니다.
     const { data, error } = await supabase
       .from('products')
       .insert([
@@ -49,23 +49,49 @@ const PostForm = ({ imgFiles }: PropsType) => {
         },
       ])
       .select();
-
+    // post_id && createdAt를 가져와서 storage의 고유 경로로 사용합니다.
     const post_id = data?.[0].id!;
-    await postImagesFromStorage(post_id, info.imgFiles);
+    const date = data?.[0].createdAt!;
+    await postImagesFromStorage(post_id, info.imgFiles, date);
 
     if (error) {
       throw console.log(error);
     }
   };
 
-  const postImagesFromStorage = async (id: number, files: File[]) => {
-    if (files) {
-      const { data, error } = await supabase.storage.from('product-images').upload('asdf/as/asdfasdf/asdf', file);
-      console.log(data, error);
-    }
+  const postImagesFromStorage = async (id: number, files: File[], date: string) => {
+    // 이미지가 Array 형태로 담겨져 있으므로 promise All을 사용하려고 변수에 담았습니다.
+    const uploadImage = files.map(async (file) => {
+      // nano id를 사용해서 이미지 고유 이름으로 넣습니다.
+      const imageName = nanoid();
+      const { data } = await supabase.storage
+        .from('product-images')
+        .upload(`${userId}/${id}/${date}/${imageName}`, file);
+      return data;
+    });
+
+    // Promise.all로 처리하여 urlPath라는 변수에 담습니다.
+    const urlPath = await Promise.all(uploadImage);
+
+    // getPublicUrl이라는 메소드가 동기 함수입니다;; 당황함 그래서 그냥 async await을 사용하지 않았습니다.
+    const urls = urlPath.map((url) => {
+      const { data } = supabase.storage.from('product-images').getPublicUrl(`${url?.path}`);
+      return data.publicUrl;
+    });
+
+    // product에다가 다시 넣어줬습니다.
+    const { data, error } = await supabase
+      .from('products')
+      .update({
+        productImg: urls,
+      })
+      .eq('id', id) // product_id를 찾는 eq입니다.
+      .select();
   };
+
+  // 사용자의 고유 아이디 uid를 가져옵니다.
   useEffect(() => {
-    getUserSession().then((userSession) => {
+    getUserSession().then(async (userSession) => {
       if (userSession.session) {
         setUserId(userSession.session.user.id);
       }
