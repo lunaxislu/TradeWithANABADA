50c9b4e8a0031f8ed7b343e8c1b3ccc9c37d4397
최문길 <cmk0905@naver.com>
2023-12-28 14:28:47 +0900

    chore: 상품등록하기 버튼 css까지 완성

--- src/components/post/form/PostForm.tsx
+++ src/components/post/form/PostForm.tsx
@@ -1,11 +1,15 @@
+import { FormEvent } from 'react';
 import * as St from './PostForm.styled';
 import FormButton from './formButton/FormButton';
 import HashTag from './hashTag/HashTag';
 import ProductInfo from './productInfo/ProductInfo';
 const PostForm = () => {
+  const preventDefaultEvent = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <St.Container>
-      <St.Form>
+      <St.Form onSubmit={preventDefaultEvent}>
         <ProductInfo />
         <HashTag />
         <FormButton />
