71186620784e12ff16957a3e303d80c59c8c2025
j-yesung <hisung-ah@kakao.com>
2023-12-29 00:13:10 +0900

    feat: 무한 스크롤 기능 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,81 @@
-import { useEffect } from 'react';
-import { getUserData, getUserSession } from '../API/supabase.api';
-import { useAuth } from '../hooks/userHook/useAuth';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+import { getProducts } from '../API/supabase.api';
 
 const Home = () => {
-  // 로그인 세션 테스트 코드
-  const { logout } = useAuth();
+  const loadMoreRef = useRef<HTMLDivElement>(null);
+
+  const {
+    data: products,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    initialPageParam: 1,
+    queryKey: ['products'],
+    queryFn: async ({ pageParam = 1 }) => await getProducts(pageParam),
+    getNextPageParam: (lastPage, allPage) => {
+      if (lastPage.length === 0) {
+        return;
+      } else {
+        return allPage.length + 1;
+      }
+    },
+  });
+
   useEffect(() => {
-    const getUserInfo = async () => {
-      const s = await getUserData();
-      console.log('getUserInfo: ', s);
+    if (!hasNextPage) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          if (hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+          }
+        }
+      },
+      { threshold: 0.3 },
+    );
+
+    const el = loadMoreRef.current;
+    if (el) {
+      observer.observe(el);
+    }
+
+    return () => {
+      if (el) {
+        observer.unobserve(el);
+      }
     };
-    const getUserToken = async () => {
-      const s = await getUserSession();
-      console.log('getUserToken: ', s);
-    };
-    getUserInfo();
-    getUserToken();
-  }, []);
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
 
   return (
-    <>
-      <h1>Home</h1>
-      <button onClick={() => logout()}>로그아웃</button>
-    </>
+    <div>
+      {products?.pages.map((page, i) => {
+        if (page.length === 0) {
+          return <></>;
+        }
+        return (
+          <div
+            style={{
+              height: '500px',
+              border: '1px solid black',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+            key={i}
+          >
+            {page?.map((product) => <div key={product.title}>{product.content}</div>)}
+          </div>
+        );
+      })}
+
+      {/* Loading spinner */}
+      {isFetchingNextPage && <p>Loading...</p>}
+
+      {/* 옵저버 역할 */}
+      {hasNextPage && <div ref={loadMoreRef}></div>}
+    </div>
   );
 };
 
