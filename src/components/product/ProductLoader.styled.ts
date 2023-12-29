d4790726a73c3af3904f99e9b9c9daa912dc4379
j-yesung <hisung-ah@kakao.com>
2023-12-30 03:33:05 +0900

    chore: 검색 결과 페이지 스타일링

--- src/components/product/ProductLoader.styled.ts
+++ src/components/product/ProductLoader.styled.ts
@@ -62,6 +62,10 @@ export const HeartBox = styled.section`
   align-items: center;
 `;
 
+export const Date = styled.div`
+  font-size: 1.2rem;
+`;
+
 export const Content = styled.div`
   gap: 1rem;
   display: flex;
@@ -74,9 +78,6 @@ export const Content = styled.div`
   p:nth-of-type(2) {
     font-size: 1.5rem;
   }
-  p:nth-of-type(3) {
-    font-size: 1.5rem;
-  }
 
   div {
     display: flex;
@@ -86,6 +87,7 @@ export const Content = styled.div`
     p:nth-of-type(1) {
       padding: 0.5rem;
       font-size: 1.5rem;
+      font-weight: 700;
       border-radius: 0.7rem;
       background-color: #c3e2c2;
     }
@@ -94,95 +96,3 @@ export const Content = styled.div`
     }
   }
 `;
-// import styled from 'styled-components';
-
-// export const Container = styled.div`
-//   padding: 5rem;
-//   display: grid;
-//   place-items: center;
-// `;
-
-// export const TitleWrapper = styled.div`
-//   display: grid;
-//   width: 100%;
-//   padding: 2rem;
-//   grid-auto-flow: column;
-
-//   span {
-//     font-size: 2.5rem;
-//     font-weight: 700;
-//   }
-// `;
-
-// export const List = styled.ul`
-//   gap: 3rem;
-//   display: grid;
-//   grid-template-columns: repeat(4, 1fr);
-
-//   li {
-//     gap: 2rem;
-//     padding: 10px;
-//     display: grid;
-//     position: relative;
-//     border-radius: 1rem;
-//     grid-auto-flow: column;
-//     border: 0.1rem solid #ccc;
-//     box-shadow: 2px 2px 8px #ccc;
-//   }
-
-//   img {
-//     width: 100%;
-//     height: auto;
-//     object-fit: cover;
-//   }
-
-//   div {
-//     font-size: 1.8rem;
-//   }
-
-//   span {
-//     font-size: 1.8rem;
-//   }
-// `;
-
-// export const HeartBox = styled.section`
-//   gap: 0.5rem;
-//   top: -17rem;
-//   right: 2rem;
-//   display: grid;
-//   position: absolute;
-//   place-items: center;
-// `;
-
-// export const Content = styled.div`
-//   gap: 1rem;
-//   display: grid;
-//   grid-auto-flow: row;
-
-//   p:nth-of-type(1) {
-//     font-size: 2.5rem;
-//     font-weight: 700;
-//   }
-//   p:nth-of-type(2) {
-//     font-size: 1.5rem;
-//   }
-//   p:nth-of-type(3) {
-//     font-size: 1.5rem;
-//   }
-
-//   div {
-//     display: grid;
-//     font-size: 1.5rem;
-//     grid-auto-flow: column;
-
-//     p:nth-of-type(1) {
-//       padding: 0.5rem;
-//       font-size: 1.5rem;
-//       border-radius: 0.7rem;
-//       background-color: #c3e2c2;
-//     }
-//     p:nth-of-type(2) {
-//       padding: 0.5rem;
-//     }
-//   }
-// `;
