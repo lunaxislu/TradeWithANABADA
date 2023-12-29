f0735c0a85ee32c96eebd33395dd101b171a2b83
최문길 <cmk0905@naver.com>
2023-12-30 05:17:06 +0900

    feat: state값 정리함

--- src/components/detail/sale/ButtonGroup/Button.tsx
+++ src/components/detail/sale/ButtonGroup/Button.tsx
@@ -16,15 +16,22 @@ const Button = ({ post_id, user_id }: PropsType) => {
   useEffect(() => {
     findLike(user_id, post_id).then((result) => {
       if (result?.[0]) {
+        setIsLike(true);
         likeIdRef.current = result?.[0].id!;
       }
     });
     userIdRef.current = user_id;
   }, [user_id]);
-
+  console.log(user_id);
+  console.log('likeRef', likeRef);
+  console.log('likeId', likeIdRef);
+  console.log('userIdRef', userIdRef);
   //  unmount 상태에서 clean up function에 useState를 사용하려면 ref 사용하자
   // useState의 초기값만 cleanup function에는 들어가게 된다.
   useEffect(() => {
+    //     console.log('likeRef', likeIdRef);
+    //     console.log('likeId', likeIdRef);
+    //     console.log('userIdRef', userIdRef);
     return () => {
       if (likeRef.current && likeIdRef.current === null) {
         registLike(userIdRef.current, post_id);
@@ -39,6 +46,7 @@ const Button = ({ post_id, user_id }: PropsType) => {
       <St.LikeButton
         $like={isLike}
         onClick={() => {
+          console.log(likeRef);
           likeRef.current = !isLike;
           setIsLike(!isLike);
         }}
