8c1bbaac8188988f0aa609a95cea90832546305e
j-yesung <hisung-ah@kakao.com>
2023-12-31 00:38:26 +0900

    chore: 프로필 페이지 스타일링

--- src/components/profile/ProfileProductList.tsx
+++ src/components/profile/ProfileProductList.tsx
@@ -1,59 +1,94 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { getLatestProducts, getPopularProducts } from '../../API/supabase.api';
-import { ProductData } from '../home/HomeProductList';
-import ProductCard from '../ui/productCard/ProductCard';
+import { getPopularProducts } from '../../API/supabase.api';
+import { displayCreateAt } from '../../utils/date';
+import { Button } from '../ui/Button';
 import * as St from './Profile.styled';
-import ProfileBtn from './ProfileBtn';
 
 type ProductListProps = {
-  type: string;
-  uid: string;
-};
-
-type ProductSectionInfoType = {
+  product_id: number;
   title: string;
-  getProductHandler: (arg1: number) => Promise<ProductData[]>;
-};
-const productSectionInfos: Record<string, ProductSectionInfoType> = {
-  likes: {
-    title: '나의 찜 목록',
-    getProductHandler: getPopularProducts,
-  },
-  sales: {
-    title: '나의 판매 목록',
-    getProductHandler: getLatestProducts,
-  },
-};
-const ProfileProductList = ({ type, uid }: ProductListProps) => {
-  const [products, setProducts] = useState<ProductData[]>();
-  // console.log(uid);
+  content: string;
+  created_at: string;
+  price: string;
+  product_img: string[];
+  user_id: string;
+  like_count: number;
+  hash_tags: string[];
+  category1_name: string;
+  category2_name: string;
+}[];
 
-  const getProductsData = async () => {
-    const data = await productSectionInfos[type].getProductHandler(4);
-    // console.log(data);
-    const filteredData = data.filter((item) => {
-      return item.user_id === uid;
-    });
-    // console.log(filteredData);
-    setProducts(data);
-  };
+const ProfileProductList = () => {
+  const [products, setProducts] = useState<ProductListProps>([]);
 
   useEffect(() => {
-    getProductsData();
+    const getProductList = async () => {
+      const data = await getPopularProducts(4);
+      console.log('data: ', data);
+      setProducts(data);
+    };
+    getProductList();
   }, []);
+
+  const showZZimList = () => {
+    // 클릭하면 찜 목록 보여주기
+  };
+  const showSalesList = () => {
+    // 클릭하면 판매 목록 보여주기
+  };
+  const deleteProduct = () => {
+    // 클릭하면 상품 삭제
+  };
+
   return (
     <St.ProductListSection>
-      {products?.map((item, index) => <ProfileBtn key={index} item={item.user_id} />)}
-      {/* <List /> */}
-      <div>
-        <h2>{productSectionInfos[type].title}</h2>
+      <St.ListTitle>
+        <div color="success" onClick={showZZimList}>
+          찜 목록
+        </div>
+        <div color="success" onClick={showSalesList}>
+          판매 목록
+        </div>
+      </St.ListTitle>
 
-        <Link to={''}>누군가의 페이지로 가기</Link>
-      </div>
-      <St.ProductListArea>
-        <ul>{products?.map((product) => <ProductCard key={product.product_id} productInfo={product} />)}</ul>
-      </St.ProductListArea>
+      {/* 리스트 영역 */}
+      <St.ListWrapper>
+        <ul>
+          {products?.map((item, i) => {
+            const date = displayCreateAt(item.created_at);
+            return (
+              <li key={i}>
+                <div>
+                  <St.ListImage>
+                    {item.product_img && item.product_img.length > 0 ? (
+                      <img src={item.product_img[0]} alt="" />
+                    ) : (
+                      <img src="" alt="" />
+                    )}
+                  </St.ListImage>
+
+                  <St.PostsWrapper>
+                    <p>제목: {item.title}</p>
+                    <p>내용: {item.content}</p>
+                    <div>
+                      <p>10,000</p>
+                      <p>원의 가치</p>
+                    </div>
+                  </St.PostsWrapper>
+
+                  <St.PriceWrapper>
+                    <span>{date}</span>
+                    <Button color="primary" onClick={deleteProduct}>
+                      삭제
+                    </Button>
+                  </St.PriceWrapper>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </St.ListWrapper>
+      <St.ProductListArea></St.ProductListArea>
     </St.ProductListSection>
   );
 };
