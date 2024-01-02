2a31706126f194d81ed0d7407e5f3ca6a3ece9da
j-yesung <hisung-ah@kakao.com>
2024-01-02 18:10:41 +0900

    feat: 이미지 최적화 적용

--- src/components/common/product/Form/imageForm/ImageForm.tsx
+++ src/components/common/product/Form/imageForm/ImageForm.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { listToBlob } from '../../../../../API/supabase.api';
 
+import FileResizer from 'react-image-file-resizer';
 import { CommonProductInfoType } from '../Form';
 import * as St from './ImageForm.styled';
 import BigImgCard from './bigImgCard/BigImgCard';
@@ -16,13 +17,28 @@ const ImageForm = ({ imgFiles, setImgFiles, productInfo }: CommonImageInputType)
   const [imageIndex, setImageIndex] = useState(0);
   const inputFileRef = useRef<HTMLInputElement>(null);
 
-  const previewImages = (e: ParamOfEventType) => {
+  const previewImages = async (e: ParamOfEventType) => {
     if (showImages.length >= 5) return alert('5개가 최대입니다.');
 
     const imageFile = e.target.files || [];
-    const currentImageUrl = URL.createObjectURL(imageFile[0]);
+
+    const resizeFile = (file: File) =>
+      new Promise((res) => {
+        FileResizer.imageFileResizer(
+          file, // 파일
+          1500, // 최대 넓이
+          1500, // 최대 높이
+          'WEBP', // 포맷팅 : JPEG, PNG or WEBP.
+          20, // 이미지 퀄리티
+          0, // 모서리 라운드
+          (uri) => res(uri), // 응답 uri
+          'file', // outputType : Can be either base64, blob or file.(Default type is base64)
+        );
+      });
+    const resize = await resizeFile(imageFile[0]);
+    const currentImageUrl = URL.createObjectURL(resize as Blob);
     setShowImages((prev) => [...prev, currentImageUrl]);
-    setImgFiles((pre) => [...pre, e.target.files![0]]);
+    setImgFiles((pre) => [...pre, resize as Blob]);
   };
   const deletePreviewImage = (id: number) => {
     const editImages = showImages.filter((_, idx) => idx !== id);
@@ -51,7 +67,7 @@ const ImageForm = ({ imgFiles, setImgFiles, productInfo }: CommonImageInputType)
     if (productInfo) {
       setShowImages(productInfo.product_img);
     }
-  }, []);
+  }, [productInfo]);
 
   // 수정하기 컴포넌트에 사용되는 useEffect
   useEffect(() => {
@@ -69,7 +85,7 @@ const ImageForm = ({ imgFiles, setImgFiles, productInfo }: CommonImageInputType)
     //     setImgFiles(result)
     //   }
     // }
-  }, []);
+  }, [productInfo, setImgFiles]);
   return (
     <St.Container>
       <BigImgCard showImages={showImages} imageIndex={imageIndex} />
